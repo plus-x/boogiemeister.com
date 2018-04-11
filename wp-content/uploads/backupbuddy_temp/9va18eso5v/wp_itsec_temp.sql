f3ed6c719b72a1beb404479b789a538e592a0532
plus-x <carlos@plu.sx>
2018-04-11 12:00:54 -0500

    auto update

--- wp-content/uploads/backupbuddy_temp/9va18eso5v/wp_itsec_temp.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `wp_itsec_temp` (  `temp_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,  `temp_type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,  `temp_date` datetime NOT NULL,  `temp_date_gmt` datetime NOT NULL,  `temp_host` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,  `temp_user` bigint(20) unsigned DEFAULT NULL,  `temp_username` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,  PRIMARY KEY (`temp_id`),  KEY `temp_date_gmt` (`temp_date_gmt`),  KEY `temp_host` (`temp_host`),  KEY `temp_user` (`temp_user`),  KEY `temp_username` (`temp_username`)) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40000 ALTER TABLE `wp_itsec_temp` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wp_itsec_temp` ENABLE KEYS */;
