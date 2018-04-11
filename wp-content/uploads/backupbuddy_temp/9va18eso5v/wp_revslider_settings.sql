687ad6827c0a1ef744869561d49db835437da59d
plus-x <carlos@plu.sx>
2018-04-11 00:00:39 -0500

    auto update

---
+++ wp-content/uploads/backupbuddy_temp/9va18eso5v/wp_revslider_settings.sql
@@ -0,0 +1,3 @@
+CREATE TABLE `wp_revslider_settings` (  `id` int(9) NOT NULL AUTO_INCREMENT,  `general` text NOT NULL,  `params` text NOT NULL,  PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40000 ALTER TABLE `wp_revslider_settings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wp_revslider_settings` ENABLE KEYS */;
