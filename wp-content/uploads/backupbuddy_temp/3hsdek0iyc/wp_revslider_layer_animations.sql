99082cefd21811a31350558a2f2f55ea879a4558
plus-x <carlos@plu.sx>
2018-04-19 12:00:44 -0500

    auto update

--- wp-content/uploads/backupbuddy_temp/3hsdek0iyc/wp_revslider_layer_animations.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `wp_revslider_layer_animations` (  `id` int(9) NOT NULL AUTO_INCREMENT,  `handle` text NOT NULL,  `params` text NOT NULL,  PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40000 ALTER TABLE `wp_revslider_layer_animations` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wp_revslider_layer_animations` ENABLE KEYS */;
--- wp-content/uploads/backupbuddy_temp/3hsdek0iyc/wp_revslider_sliders.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `wp_revslider_sliders` (  `id` int(9) NOT NULL AUTO_INCREMENT,  `title` tinytext NOT NULL,  `alias` tinytext,  `params` text NOT NULL,  PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40000 ALTER TABLE `wp_revslider_sliders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wp_revslider_sliders` ENABLE KEYS */;
