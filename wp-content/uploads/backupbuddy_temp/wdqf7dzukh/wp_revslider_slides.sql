2b9d423e5e39adcdd75756c0ca81b39b6d20084e
plus-x <carlos@plu.sx>
2018-04-27 00:00:47 -0500

    auto update

---
+++ wp-content/uploads/backupbuddy_temp/wdqf7dzukh/wp_revslider_slides.sql
@@ -0,0 +1,3 @@
+CREATE TABLE `wp_revslider_slides` (  `id` int(9) NOT NULL AUTO_INCREMENT,  `slider_id` int(9) NOT NULL,  `slide_order` int(11) NOT NULL,  `params` text NOT NULL,  `layers` text NOT NULL,  PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40000 ALTER TABLE `wp_revslider_slides` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wp_revslider_slides` ENABLE KEYS */;
